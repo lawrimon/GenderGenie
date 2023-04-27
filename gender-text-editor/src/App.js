6da5d4be3972d081c2d826ab974b6da783a69a23
lawrimon <81292176+lawrimon@users.noreply.github.com>
2023-04-27 15:00:10 +0200

    feat: updated the UI

--- gender-text-editor/src/App.js
+++ gender-text-editor/src/App.js
@@ -1,37 +1,62 @@
 import React, { useState } from 'react';
 import './App.css';
+import genderLogo from './gender_logo.png';
+
 
 const App = () => {
+  const [showContainer, setShowContainer] = useState(false);
+
   const [text, setText] = useState('');
   const [highlightWords, setHighlightWords] = useState([]);
   const [showPopup, setShowPopup] = useState(false);
   const [keyvalues, setKeyValues] = useState([]);
 
   const [selectedKeyword, setSelectedKeyword] = useState('');
-const [selectedValue, setSelectedValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState('');
 
-const handleHighlightClick = (keyword, value) => {
-  setSelectedKeyword(keyword);
-  setSelectedValue(value);
-  setShowPopup(true);
-};
+  const [firstInput, setFirstInput] = useState('');
+  const [secondInput, setSecondInput] = useState('');
+
+  const suggestionTextfield = () => {
+    console.log('First Input:', firstInput);
+    console.log('Second Input:', secondInput);
+    let suggestion = { firstInput: secondInput }
+
+    fetch('http://141.31.86.15:8000/postSuggestion', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(suggestion)
+    })
+      .then(res => res.json())
+      .then(response => console.log('Success:', JSON.stringify(response)))
+      .catch(error => console.error('Error:', error));
+  };
+
+  const handleHighlightClick = (keyword, value) => {
+    setSelectedKeyword(keyword);
+    setSelectedValue(value);
+    setShowPopup(true);
+  };
 
   const handleTextChange = (event) => {
     setText(event.target.value);
-    console.log(text,"ist event change")
+    console.log(text, "ist event change")
   };
 
   const handleSubmit = () => {
-    fetch('http://192.168.0.135:8000/postText', {
+    setShowContainer(true);
+    fetch('http://141.31.86.15:8000//postText', {
       method: 'POST',
-      body: JSON.stringify({"text":text}),
+      body: JSON.stringify({ "text": text }),
       headers: { 'Content-Type': 'application/json' },
     })
-    .then(response => response.json())
-    .then(data => {
-      if (data.message) {
+      .then(response => response.json())
+      .then(data => {
+        if (data.message) {
           console.log(data.message)
-          if (data.message ==='[]'){
+          if (data.message === '[]') {
             console.log("Im IF")
             setText("Sie haben alles gender-neutral verfasst! :)")
             return
@@ -41,41 +66,69 @@ const handleHighlightClick = (keyword, value) => {
           console.log(keyvalues);
           setHighlightWords(keywords);
           setKeyValues(keyvalues);
-      }
-    })    
-    .catch(error => {
-      console.error(error);
-    });
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
   };
+
+  const downloadTxtFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob([text], { type: 'text/plain' });
+    element.href = URL.createObjectURL(file);
+    element.download = "Output.txt";
+    document.body.appendChild(element);
+    element.click();
+  }
 
   function extractKeywords(daten) {
     console.log("in extract")
     console.log(daten)
     let keywords = []
     daten.forEach(obj => {
-        Object.keys(obj).forEach(key => {
-            keywords.push(key)
-        });
+      Object.keys(obj).forEach(key => {
+        keywords.push(key)
+      });
     });
     return keywords
   };
 
-  function extractValues(daten){
+  function extractValues(daten) {
     let keyValuePairs = []
     daten.forEach(obj => {
-        Object.entries(obj).forEach(entry => {
-            keyValuePairs.push({key: entry[0],value: "  " + entry[1]})
-        });
+      Object.entries(obj).forEach(entry => {
+        keyValuePairs.push({ key: entry[0], value: "  " + entry[1] })
+      });
     });
     return keyValuePairs
   };
 
-  
+
   const handleClick = () => {
     setShowPopup(true);
   };
-  
-  
+
+  const handleWordClick = word => {
+    console.log(word)
+    setText(prevText => prevText.replace(selectedKeyword, word))
+    //setText(prevText => prevText.replace(selectedKeyword,  (<span key={word} style={{color: 'blue', cursor: 'pointer'}}>
+    //{word}
+    //</span>).toString()));
+    //setSelectedKeyword(word);
+    console.log(text)
+    //highlight_keyword(text, word, handleWordClick, "green")
+    //const newText = highlight_keyword(text, word)
+    //setText(newText);
+    //console.log(newText)
+    let result = highlight(text, [word], handleHighlightClick)
+    console.log(result)
+    //setText(text,newText)
+    // IN text replace selected word with word!!!
+    // Hier Logik einbauen um die Wörter im Text zu ersetzen
+  };
+
+
   const highlight = (text, words, handleClick) => {
     let result = [text];
     words.forEach(word => {
@@ -86,7 +139,7 @@ const handleHighlightClick = (keyword, value) => {
             const endIndex = startIndex + word.key.length;
             return [
               item.slice(0, startIndex),
-              <span key={word.key} style={{color: 'red', cursor: 'pointer'}} onClick={() => handleClick(word.key, word.value)}>
+              <span key={word.key} style={{ color: 'red', cursor: 'pointer' }} onClick={() => handleClick(word.key, word.value)}>
                 {word.key}
               </span>,
               item.slice(endIndex)
@@ -101,19 +154,55 @@ const handleHighlightClick = (keyword, value) => {
     });
     return result;
   };
-  
 
-  
+  const highlight_keyword = (text, word) => {
+    console.log("this text,", text)
+
+    word = word.replace(/\s+/g, '');
+    console.log("this word,", word)
+    console.log("this wordkey,", word.key)
+    let result = [text];
+    result = result.flatMap(item => {
+      let itemo = item.replace(/\s+/g, '');
+
+      if (typeof itemo === "string") {
+        const startIndex = itemo.indexOf(word);
+        console.log(startIndex)
+        if (startIndex >= 0) {
+          const endIndex = startIndex + word.length;
+          console.log("im if")
+          return [
+            itemo.slice(0, startIndex),
+            <span key={word} style={{ color: 'green', cursor: 'pointer' }}>
+              {word}
+            </span>,
+            itemo.slice(endIndex)
+          ];
+        } else {
+          return [item];
+        }
+      } else {
+        return [item];
+      }
+    });
+    return result;
+  };
+
+
+
+
+
+
   const handleUpload = (event) => {
     // prevent default behavior of form submission
     event.preventDefault();
-  
+
     // access the first file in the input field
     const file = event.target.files[0];
-  
+
     // create a new instance of FileReader
     const reader = new FileReader();
-  
+
     // handle the load event after reading the file
     reader.onload = (e) => {
       // set the contents of the file as the text value
@@ -121,83 +210,90 @@ const handleHighlightClick = (keyword, value) => {
       // call the handleSubmit function
       handleSubmit();
     };
-  
+
     // start reading the file as text
     reader.readAsText(file);
   };
 
-  
+
   const uploadSubmit = (event) => {
     // call the handleSubmit function
     handleSubmit(event);
   }
-  
-  
-  
-  
+
+
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>-Gender-API-</h1>
+        <img src={genderLogo} alt="Image"   style={{ 
+          marginTop: '20px', 
+          marginBottom: '80px', 
+          width: '40%', 
+          height: 'auto',
+          borderRadius: '10px',
+          filter: 'contrast(80%)',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+          backgroundColor: '#f2f2f2'
+        }} 
+      />
         <div>
-        <textarea 
-       className='big-text-field'
-        type="text"
-        rows={10}
-         onChange={handleTextChange} value={text} 
-       />
-<div style = {{padding: '20px'}}><button style ={{
-  width:'60px', 
-  height:'40px',
-  borderradius: '10px',
-  backgroundcolor: '#007bff',
-  color: 'white',
-  fontsize: '16px',
-  border: 'none',
-  cursor: 'pointer',
-  }}
-  onClick={handleSubmit}>Submit</button> </div>
- <input
-      type="file"
-      accept=".txt"
-      style={{left: '60%', padding: '20px'}}
-      onChange={handleUpload}
-    />
-        <div style={{ display: 'flex' , padding: '20px', backgroundColor: 'gray', backgroundClip: 'padding-box' }}>
-        <div style = {{padding: '20px'}} >
-  {highlight(text, keyvalues, handleHighlightClick)}
-</div>
- {showPopup && (
-  <div style={{ 
-    position: 'relative', 
-    color: 'black', 
-    backgroundColor: 'white', 
-    padding: '15px', 
-    width: '120%', 
-    left: '0',
-    height: '250px',
-    top: '0', 
-    overflow: 'scroll',
-    whitespace: 'prewrap',
-    wordwrap: 'breakword'
-  }}>
-    <div >      
-      <b>{selectedKeyword}:</b> {selectedValue}
-    </div>
-  </div>  
-)}
+          <textarea
+            className='big-text-field'
+            type="text"
+            rows={10}
+            onChange={handleTextChange} value={text}
+          />
 
+          <div>
+            <button className="apple-btn" onClick={downloadTxtFile}>Download</button>
+            <button className="apple-btn" onClick={handleSubmit}>Submit</button>
+            <label for="file-input" className="apple-btn" >Select .txt file</label>
+            <input type="file" id="file-input" accept=".txt" onChange={handleUpload} />
+          </div>
 
-</div>
+          {showContainer && <div className='container'>
+            <div className='Apple-style-div'>
+              {highlight(text, keyvalues, handleHighlightClick)}
+            </div>
+
+            {showPopup && (
+              <div className='Popup'>
+                <div >
+                  <b>{selectedKeyword}</b>:{selectedValue.split(",").map((word, index) => (
+                    <span key={index} onClick={() => handleWordClick(word)}>
+                      {word + ","}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            )}
 
 
 
+          </div> }
 
-
+          <h2> Suggestion Box</h2>
+          <div className="suggestion-box">
+            <div className="input-wrapper">
+              <input type="text" placeholder="Ungegendertes Wort" value={firstInput} onChange={(e) => setFirstInput(e.target.value)} />
+            </div>
+            <div className="input-wrapper">
+              <input type="text" placeholder="Alternative" value={secondInput} onChange={(e) => setSecondInput(e.target.value)} />
+            </div>
+            <div className="button-wrapper">
+              <button onClick={suggestionTextfield}>Submit</button>
+            </div>
+          </div>
 
         </div>
       </header>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50px', backgroundColor: '#f2f2f2', padding: "10px" }}>
+        <p style={{ margin: 0, fontSize: '12px' }}>
+          Ⓒ GenderInator 2023<br /> Keine Verwendung für kommerzielle Zwecke. <br /> Datensatz erhalten durch https://geschicktgendern.de
+        </p>
+      </div>
     </div>
   );
 };
--- genderIO/gender_algorithm.py
+++ genderIO/gender_algorithm.py
@@ -1,8 +1,14 @@
 from spacy.tokens import Token
 import re
+import spacy
+import pandas as pd
+from pymongo import MongoClient
+
 
 # Add 'gender' extension to Token with default value of None
 Token.set_extension('gender', default=None)
+nlp = spacy.load("de_core_news_sm")
+
 
 import pandas as pd
 
@@ -42,6 +48,16 @@ def check_dict(string, my_dict):
     else:
         return None
 
+def check_database(string, collection):
+    word = find_word_database(string, collection)
+    if word is not None:
+        array = split_dict(string, word)
+        star_sg = second_check(string)
+        array.append(star_sg)
+        return ({string: array})
+    else:
+        pass
+
 def replace_words(file_path, word_list):
     with open(file_path, 'r') as file:
         content = file.read()
@@ -52,17 +68,54 @@ def replace_words(file_path, word_list):
     with open(file_path, 'w') as file:
         file.write(content)
 
+def find_word_position_in_string(string, word):
+    lines = string.split('\n')
+    for line_number, line in enumerate(lines):
+        if word in line:
+            print(f"The word '{word}' was found on line {line_number + 1}." )
+            return
+    return f"The word '{word}' was not found in the string."
 
-
-
+def find_word_database(word, collection):
+    document = collection.find_one({"word": word})
+    # Check if the document was found
+    if document:
+        definition = document["definition"]
+        return {word : definition}
+    else:
+        pass
 
 def second_check(word):
     """
     Given a word, return the results of the replace_token_pl and replace_token_sg functions
     """
     word_sg = replace_token_sg(word)
-    word_pl = replace_token_pl(word)
-    return word_sg, word_pl
+    #word_pl = replace_token_pl(word)
+    return word_sg
+
+def check_wordending(word):
+    doc = nlp(word)
+    if not doc:
+        print("kein DOC")
+        return None
+    token = doc[0]
+    print(word)
+    # Get the first token
+    # Check if the first token is a noun
+    if word == "Lügner":
+        print("asahduh")
+        try:
+            print(token.pos_)
+        except:
+            pass
+    try:
+        if token.pos_ == "NOUN":
+                if word.endswith("er") or word.endswith("en"):
+                    return {word: word + "*in"}
+        else:
+            pass
+    except:
+        pass
 
 
 def filter_non_letters(word):
@@ -98,13 +151,75 @@ def replace_token_pl(word):
     return word
 
 def mainfunction(text):
-    gender_dict = init_dict()
+    client = MongoClient("mongodb+srv://admin:admin@genderinator.psfvydj.mongodb.net/?retryWrites=true&w=majority")
+    db = client["GenderInator"]
+    collection = db["GenderRegeln"]
+
+    #gender_dict = init_dict()
     result = []
+    #upload_excel(gender_dict)
     for token in text.split():
         token = filter_non_letters(token)
-        dict_element = check_dict(token, gender_dict)
+        #find_word_position_in_string(text, token)
+        #test_person(token)
+        dict_element = check_database(token, collection)
         if dict_element:
             result.append(dict_element)
+        else:
+            result_word = check_wordending(token)
+            if result_word is not None:
+                result.append(result_word)
+        
     print(result)
     return result
 
+def upload_excel(dict_array):
+
+    print(dict_array)
+    # Schritt 2: Verbinden Sie sich mit MongoDB
+    client = MongoClient("mongodb+srv://admin:admin@genderinator.psfvydj.mongodb.net/?retryWrites=true&w=majority")
+    db = client["GenderInator"]
+    collection = db["GenderRegeln"]
+    documents = [{"word": key, "definition": value} for key, value in dict_array.items()]
+
+    # Schritt 3: Konvertieren und speichern Sie die Daten
+    #collection.insert_many(documents)
+
+
+def detect_person(text):
+    doc = nlp(text)
+    for token in doc:
+        print("Token:", token)
+        print("Text:", token.text)
+        print("Lemma:", token.lemma_)
+        print("POS:", token.pos_)
+        print("Tag:", token.tag_)
+        print("Dep:", token.dep_)
+        print("Shape:", token.shape_)
+        print("Alpha:", token.is_alpha)
+        print("Stop:", token.is_stop)
+        try:
+           print("Number:",token.tag_)
+           if "PLURAL" in token.tag_:
+                print("NIEMALS")
+        except:
+            pass
+        print("\n")
+    for ent in doc.ents:
+        if ent.label_ == "PERSON" or ent.label_ == "PROPN" or ent.label_ == "NOUN":
+            return True
+    return False
+
+
+def test_person(word):
+    if detect_person(word):
+        print("The text refers to a person")
+    else:
+        return
+
+def postSuggestion(text):
+    client = MongoClient("mongodb+srv://admin:admin@genderinator.psfvydj.mongodb.net/?retryWrites=true&w=majority")
+    db = client["GenderInator"]
+    collection = db["GenderSuggestions"]
+    print("this the text",text)
+    #collection.insert_one(text)
